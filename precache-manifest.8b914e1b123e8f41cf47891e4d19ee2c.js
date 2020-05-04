e5a215208386b256aa4d011ec8aa5b4e8983128b
Yuliia <ybelova24@gmail.com>
2020-05-04 18:09:08 +0300

    Updates

---
+++ precache-manifest.8b914e1b123e8f41cf47891e4d19ee2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f6ff47b3bc77805537ed88bfb97af3",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "e93bc2fdb1902e4cc974",
+    "url": "/goit-react-hw-02-feedback/static/js/2.5ae709f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.5ae709f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5903c74b0bb74a71c0a4",
+    "url": "/goit-react-hw-02-feedback/static/js/main.7b8a9b6e.chunk.js"
+  },
+  {
+    "revision": "921f1dce578c37a88de9",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.912650e5.js"
+  }
+]);
